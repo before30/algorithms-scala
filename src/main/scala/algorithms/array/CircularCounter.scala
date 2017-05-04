86447c619ab16a1e24a9fef3b752cccaf75f326b
kimxogus <kgyoo8232@gmail.com>
2017-05-05 06:05:04 +0900

    update CircularCounter

--- src/main/scala/algorithms/array/CircularCounter.scala
+++ src/main/scala/algorithms/array/CircularCounter.scala
@@ -3,14 +3,14 @@ package algorithms.array
 
 object CircularCounter {
 
-  def apply[? <: Any](array: Array[?], skip: Int, action: (?) => Any): Unit = {
+  def apply[? <: Any](array: Array[?], skip: Int, action: (?) => Unit): Unit = {
     if (skip < 1) throw new IllegalArgumentException(s"Skip should be larger than 0 '$skip'")
 
     var idx: Int = 0
     var copiedArray: Array[?] = array.clone()
 
     while (copiedArray.length > 0) {
-      idx = (skip + idx - 1) % copiedArray.length
+      idx = (skip - 1 + idx) % copiedArray.length
       action(copiedArray.apply(idx))
       val (start, end) = copiedArray.splitAt(idx)
       (start ++ end.drop(1)).copyToArray[?](copiedArray)
