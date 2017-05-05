a2dcfd61d033183d286cbee054163868c75c4291
kimxogus <kgyoo8232@gmail.com>
2017-05-05 10:09:26 +0900

    fix test spec

--- src/test/scala/algorithms/array/CircularCounterTest.scala
+++ src/test/scala/algorithms/array/CircularCounterTest.scala
@@ -8,15 +8,15 @@ class CircularCounterTest extends UnitSpec {
   "CircularCounter" when {
 
     "skip value is larger than or equal to 1" should {
-      "works fine" in {
+      "work fine" in {
         var list = List[Int]()
-        CircularCounter[Int](List(1, 2, 3, 4, 5, 6, 7, 8, 9), 3, v => list = list ++ List(v))
+        CircularCounter[Int](List(1, 2, 3, 4, 5, 6, 7, 8, 9), 3, v => list ++= List(v))
         assert(list == List(3, 6, 9, 4, 8, 5, 2, 7, 1))
       }
     }
 
     "skip value is less than 1" should {
-      "throws IllegalArgumentException" in {
+      "throw IllegalArgumentException" in {
         intercept[IllegalArgumentException] {
           CircularCounter[Int](List(), -1, _ => {})
         }
