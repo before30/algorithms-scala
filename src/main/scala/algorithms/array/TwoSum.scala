b9f5e8945d4d7e4db050f2bf3d1b3256140e4edf
kimxogus <kgyoo8232@gmail.com>
2017-05-05 10:40:50 +0900

    add comments

--- src/main/scala/algorithms/array/TwoSum.scala
+++ src/main/scala/algorithms/array/TwoSum.scala
@@ -1,5 +1,12 @@
 package algorithms.array
 
+/**
+  * Given an array of integers, return indices of the two numbers
+  * such that they add up to a specific target.
+  *
+  * You may assume that each input would have exactly one solution,
+  * and you may not use the same element twice.
+  */
 object TwoSum {
 
   def apply(list: List[Int], target: Int): List[Int] = {
