3cf91591a0d1278e8eaeed0d41632e92a2b6523d
kimxogus <kgyoo8232@gmail.com>
2017-05-05 10:46:18 +0900

    update test spec

--- src/test/scala/algorithms/array/TwoSumTest.scala
+++ src/test/scala/algorithms/array/TwoSumTest.scala
@@ -12,7 +12,11 @@ class TwoSumTest extends UnitSpec {
       val target = 5
 
       "success with List(1,2,3)" in {
-        assert(TwoSum(List(1, 2, 3), target) == List(1, 2))
+        val input: List[Int] = List(1, 2, 3)
+        val result: List[Int] = TwoSum(input, target)
+
+        assert(result == List(1, 2))
+        assert(input.apply(result.head) + input.apply(result.last) == target)
       }
 
       "fail with List(1,3,5)" in {
