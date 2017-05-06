e52954f7c47c553997740454dcd9c8742b3b8de0
kimxogus <kgyoo8232@gmail.com>
2017-05-06 17:54:59 +0900

    add implementation using updated and fix dropAndTake

--- src/main/scala/algorithms/array/RotateArray.scala
+++ src/main/scala/algorithms/array/RotateArray.scala
@@ -5,12 +5,24 @@ package algorithms.array
   */
 object RotateArray {
 
-  def apply[T](list: List[T], k: Int): List[T] = {
+  def dropAndTake[T](list: List[T], k: Int): List[T] = {
     if (k < 0) throw new IllegalArgumentException(s"k should not be negative '$k'")
 
-    val step = k % list.size
+    val step = list.size - k % list.size
 
     list.drop(step) ++ list.take(step)
+  }
+
+  def updated[T](list: List[T], k: Int): List[T] = {
+    if (k < 0) throw new IllegalArgumentException(s"k should not be negative '$k'")
+
+    var result = list.drop(0)
+
+    for ((elem, i) <- list.zipWithIndex) {
+      result = result.updated((i + k) % result.size, elem)
+    }
+
+    result
   }
 
 }
--- src/test/scala/algorithms/array/RotateArrayTest.scala
+++ src/test/scala/algorithms/array/RotateArrayTest.scala
@@ -5,22 +5,38 @@ import algorithms.UnitSpec
 
 class RotateArrayTest extends UnitSpec {
 
-  "RotateArray" when {
+  "RotateArray.dropAndTake" when {
 
     "k is larger or equal to 0" should {
       "success" in {
-        assert(RotateArray(List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 7) == List(7, 8, 9, 0, 1, 2, 3, 4, 5, 6))
+        assert(RotateArray.dropAndTake(List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 7) == List(3, 4, 5, 6, 7, 8, 9, 0, 1, 2))
       }
     }
 
     "k is less than 0" should {
       "throw IllegalArgumentException" in {
         intercept[IllegalArgumentException] {
-          RotateArray(List(), -1)
+          RotateArray.dropAndTake(List(), -1)
         }
       }
     }
+  }
 
+  "RotateArray.updated" when {
+
+    "k is larger or equal to 0" should {
+      "success" in {
+        assert(RotateArray.updated(List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 7) == List(3, 4, 5, 6, 7, 8, 9, 0, 1, 2))
+      }
+    }
+
+    "k is less than 0" should {
+      "throw IllegalArgumentException" in {
+        intercept[IllegalArgumentException] {
+          RotateArray.updated(List(), -1)
+        }
+      }
+    }
   }
 
 }
