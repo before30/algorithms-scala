91def6dbbf6a25e0faee948b20bb0f7d121d4aab
Joel <before30@coupang.com>
2017-05-08 15:57:17 +0900

    Add PlusOne

---
+++ src/main/scala/algorithms/array/PlusOne.scala
@@ -0,0 +1,10 @@
+package algorithms.array
+
+/**
+  * User: before30 
+  * Date: 2017. 5. 8.
+  * Time: AM 8:59
+  */
+object PlusOne {
+
+}
---
+++ src/test/scala/algorithms/array/PlusOneTest.scala
@@ -0,0 +1,10 @@
+package algorithms.array
+
+/**
+  * User: before30 
+  * Date: 2017. 5. 8.
+  * Time: AM 9:16
+  */
+class PlusOneTest {
+
+}
