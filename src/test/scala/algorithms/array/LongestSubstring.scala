c55d9a3635ec24294465dc442d33d7d3659b12cc
before30 <before30@gmail.com>
2017-05-15 21:49:09 +0900

    Add LongestSubstring

---
+++ src/test/scala/algorithms/array/LongestSubstring.scala
@@ -0,0 +1,20 @@
+package algorithms.array
+
+/**
+  * User: before30 
+  * Date: 2017. 5. 10.
+  * Time: PM 12:02
+  *
+  * Difficulty : Medium
+  * Given a string, find the length of the longest substring without repeating characters.
+  *
+  * Examples:
+  *  Given "abcabcbb", the answer is "abc", which the length is 3.
+  *  Given "bbbbb", the answer is "b", with the length of 1.
+  *  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+  *
+  * site : https://leetcode.com/problems/longest-substring-without-repeating-characters/#/description
+  */
+object LongestSubstring {
+
+}
